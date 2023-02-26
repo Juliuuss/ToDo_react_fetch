656abe3e4188f88fcd2886e16e1a807ef8df289d
Juliuuss <j.esp.ver@gmail.com>
2023-02-26 22:20:12 +0000

    Finzlizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,68 +1,103 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	const [inputTodo, setInputTodo] = useState("");
-	const [task, setTask] = useState([]);
-	function addTask (){
-		setTask(task.concat(inputTodo));
-		setInputTodo("");
-	}
-	fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
-      method: "PUT",
-      body: JSON.stringify(todos),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(resp => {
-        console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
-        console.log(resp.status); // el código de estado = 200 o código = 400 etc.
-        console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
-        return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
-    })
-    .then(data => {
-        //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-        console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
-    })
-    .catch(error => {
-        //manejo de errores
+  const [inputTodo, setInputTodo] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  useEffect(() => {
+    const getTasks = async () => {
+      try {
+        const response = await fetch(
+          "https://assets.breatheco.de/apis/fake/todos/user/julio93"
+        );
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        const tasks = await response.json();
+        setTasks(tasks);
+      } catch (error) {
         console.log(error);
-    });
+      }
+    };
+    getTasks();
+  }, []);
+
+  const addTask = async (e) => {
+    if (e.key === "Enter") {
+      //const newTask = { label: inputTodo, done: false };
+	  const obj = [ ...tasks,{"label": e.target.value, "done": false}]
+      const response = await fetch(
+        "https://assets.breatheco.de/apis/fake/todos/user/julio93",
+        {
+          method: "PUT",
+          body: JSON.stringify([...tasks, newTask]),
+		  mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+		setNewTask(current => [...current, inputTodo])
+        setTasks(current=> [...obj])
+        e.target.value = "";
+		setInputTodo("")
+    }
+  };
+
+  const deleteTask = async (index) => {
+	const tmp = tasks
+    tmp.splice(index, 1)
+    const response = await fetch(
+      "https://assets.breatheco.de/apis/fake/todos/user/julio93",
+      {
+        method: "PUT",
+        body: JSON.stringify(tmp),
+		mode:"cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    setTasks([...tmp])
+	 setNewTask(current =>[
+        ...current.slice(0, index),
+        ...current.slice(index + 1, current.length)
+        ]);
+  };
 
 
 
-
-
-	return (
-		<>
-			<div className="container-sm mt-2 text-center">
-				<h1>My ToDos</h1>
-				<ul class="list-group mt-5">
-					<li class="list-group-item">
-						<input type="text"
-							className="form-control border border-0"
-							placeholder="ToDos"
-							aria-label="Username"
-							aria-describedby="basic-addon1"
-							onChange={(e) => setInputTodo(e.target.value)}
-							value={inputTodo}
-							onKeyDown={(e) => e.key === "Enter" ? addTask() : null}
-						/>
-					</li>
-					{task.map((t, index) => (
-						<li className="list-group-item d-flex justify-content-between">{t}{" "}
-							<i class="fas fa-trash-alt" onClick={() => setTask(task.filter((t, currentIndex) => index != currentIndex))}></i>
-						</li>
-					))}
-					<li className="list-group-item">{task.length} Tareas Faltantes</li>
-				</ul>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="container-sm mt-2 text-center">
+        <h1>My ToDos</h1>
+        <ul className="list-group mt-5">
+          <li className="list-group-item">
+            <input
+              type="text"
+              className="form-control border border-0"
+              placeholder="ToDos"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+              onChange={(e) => setInputTodo(e.target.value)}
+              value={inputTodo}
+              onKeyDown={(e) => addTask(e)}
+            />
+          </li>
+          {tasks.map((task, index) => (
+            <li className="list-group-item d-flex justify-content-between" key={index}>
+              {task.label}
+              <i
+                className="fas fa-trash-alt"
+                onClick={() => deleteTask(index)}
+              ></i>
+            </li>
+          ))}
+          <li className="list-group-item">{tasks.length} Tareas Faltantes</li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Home;
